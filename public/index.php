df2b2341fdd7ee1fa3e2864fe479d615f09d1c84
Eduardo Romanelli <144383325+edurromanelli@users.noreply.github.com>
2025-05-16 23:42:47 +0000

    bla bla bla

--- public/index.php
+++ public/index.php
@@ -1,6 +1,10 @@
 <?php
 define("NOME_CURSO", Sistemas.de.informação);
 define("INSTITUIÇÃO", Multivix);
-$semestre_atual = 5;
+define("ANO_CORRENTE, 2024");
+$ano_inicio_curso = 2020;
+$semestre_atual = 7;
+$calculo_tempo_curso = ANO_CORRENTE - $ano_inicio_curso;
 echo "Estou no {$semestre_atual}° período do curso de " . NOME_CURSO . " na " . INSTITUICAO . ".";
+echo "Você está no curso há" .$calculo_tempo_curso. "anos" ;
 ?>
